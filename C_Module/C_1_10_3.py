3182614db5a68dcc415a64ebfdad3161276a38ad
Alexey <alexey.vronsky.studio@gmail.com>
2021-04-23 15:45:11 +0300

    Created second task for Pets Home guests in file C_1_10_3

--- C_Module/C_1_10_3.py
+++ C_Module/C_1_10_3.py
@@ -1,5 +1,5 @@
 class EWallet:
-    def __init__(self, name, surname, balance):
+    def __init__(self, name, surname, balance=""):
         self.name = name
         self.surname = surname
         self.balance = balance
@@ -10,3 +10,19 @@ class EWallet:
 
 first_client = EWallet("Иван", "Петров", "50 долларов")
 print(first_client)
+
+# Выполняем создание гостей наследуя имя и фамилию из класса EWallet
+
+
+class Guest(EWallet):
+    def __init__(self, name, surname, city, status):
+        self.city = city
+        self.status = status
+        EWallet.__init__(self, name, surname)
+
+    def __str__(self):
+        return f"\nГость: {self.name} {self.surname}, {self.city}. Статус : {self.status}"
+
+
+guest_1 = Guest("Иван", "Петров", "Москва", "Наставник")
+print(guest_1)
